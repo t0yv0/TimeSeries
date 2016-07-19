ded36f2c91b0cfd1aa4a8bbc20a055bfbf8f7d02
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2016-07-19 14:46:30 -0700

    Remove redundant

--- TimeSeries/Math.fs
+++ TimeSeries/Math.fs
@@ -7,7 +7,6 @@ let interpolateLinear (x1: float) y1 x2 y2 =
     fun x -> y1 + b * x
 
 let interpolateLinearOnDomain (toF: 'T -> float) (x1: 'T) (y1: float) (x2: 'T) (y2: float) =
-    // let toF (x: DateTime) = (x - x1).TotalDays
     toF >> interpolateLinear (toF x1) y1 (toF x2) y2
 
 let interpolateLinearOnDateTimeDomain (x1: DateTime) y1 x2 y2 =
